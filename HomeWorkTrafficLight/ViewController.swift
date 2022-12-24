276bca4d7b8847a4942f0f985cac09bb15d594ae
Kirilloao <kirilltr@list.ru>
2022-12-24 12:11:16 +0100

    Initial Commit

---
+++ HomeWorkTrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkTrafficLight
+//
+//  Created by Kirill Taraturin on 24.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
